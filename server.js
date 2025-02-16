616b2b607bb3ef13dd595beb557812357be1c704
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-16 12:30:45 +0100

    server 2

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/posts");
+
+dotenv.config();
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI, {})
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
